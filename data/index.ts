b7e8aa18731de5b494e20bb1df0dcfd351076441
Claudiu-c <claudiu.capatana1@gmail.com>
2024-09-16 05:38:51 +0300

    first commit

---
+++ data/index.ts
@@ -0,0 +1,145 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Exploring creative coding projects",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Hotel Management Site",
+    des: "Discover the art of hospitality with our intuitive hotel management platform, designed to streamline operations and enhance guest experiences effortlessly.",
+    img: "/hotels.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/mongodb.svg"],
+    link: "https://github.com/Claudiu-c/hotel-management",
+  },
+  {
+    id: 2,
+    title: "Foor Ordering Platform",
+    des: "Experience the future of dining with our seamless food ordering platform, connecting you to your favorite meals with just a few clicks",
+    img: "/food.svg",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/mongodb.svg",
+      "/nodejs.svg",
+      "/ts.svg",
+    ],
+    link: "https://github.com/adrianhajdin/zoom-clone",
+  },
+  {
+    id: 3,
+    title: "Threads App",
+    des: "Join the conversation with Threads app, where sharing thoughts and connecting with others is fast, fun, and effortless.",
+    img: "/threads.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/mongodb.svg"],
+    link: "https://github.com/adrianhajdin/ai_saas_app",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Freelance Website developer",
+    desc: "Led the dev of a web-based platform",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance Website administrator in Wordpress",
+    desc: "Administrated an e-commerce website in Wordpress",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of the frontend of a mobile app using react-native",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "L1 Support Junior Engineer + Linux & SQL internship",
+    desc: "Intern and then worked as 1st line of support at the same company",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
