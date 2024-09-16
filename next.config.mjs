5f9d6365eca50384ecd453355966fd8f1cd9e3f8
Claudiu-c <claudiu.capatana1@gmail.com>
2024-09-16 05:48:51 +0300

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+};
+
+module.exports = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+}
+
 
 export default nextConfig;
